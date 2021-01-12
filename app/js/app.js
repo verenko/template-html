7326e3dd50ee2b1be06671d9dc91f830802658c7
verenko <verenko2000@gmail.com>
2021-01-12 20:44:09 +0200

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+// require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+
+})
